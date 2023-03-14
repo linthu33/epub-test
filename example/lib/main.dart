772b67cc0d47cf9a8065888307455a294d912a35
linthu <linthu2033@gmail.com>
2023-03-14 15:46:23 +0630

    first commit

---
+++ example/lib/main.dart
@@ -0,0 +1,165 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:vocsy_epub_viewer/epub_viewer.dart';
+
+void main() async {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool loading = false;
+  Dio dio = Dio();
+  String filePath = "";
+
+  @override
+  void initState() {
+    download();
+    super.initState();
+  }
+
+  download() async {
+    if (Platform.isAndroid || Platform.isIOS) {
+      await downloadFile();
+    } else {
+      loading = false;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Vocsy Plugin E-pub example'),
+        ),
+        body: Center(
+          child: loading
+              ? Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CircularProgressIndicator(),
+                    Text('Downloading.... E-pub'),
+                  ],
+                )
+              : Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () async {
+                        print("=====filePath======$filePath");
+                        VocsyEpub.setConfig(
+                          themeColor: Theme.of(context).primaryColor,
+                          identifier: "iosBook",
+                          scrollDirection: EpubScrollDirection.ALLDIRECTIONS,
+                          allowSharing: true,
+                          enableTts: true,
+                          nightMode: true,
+                        );
+
+                        // get current locator
+                        VocsyEpub.locatorStream.listen((locator) {
+                          print('LOCATOR: $locator');
+                        });
+
+                        VocsyEpub.open(
+                          filePath,
+                          lastLocation: EpubLocator.fromJson({
+                            "bookId": "2239",
+                            "href": "/OEBPS/ch06.xhtml",
+                            "created": 1539934158390,
+                            "locations": {
+                              "cfi": "epubcfi(/0!/4/4[simple_book]/2/2/6)"
+                            }
+                          }),
+                        );
+                      },
+                      child: Text('Open Online E-pub'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () async {
+                        VocsyEpub.setConfig(
+                          themeColor: Theme.of(context).primaryColor,
+                          identifier: "iosBook",
+                          scrollDirection: EpubScrollDirection.ALLDIRECTIONS,
+                          allowSharing: true,
+                          enableTts: true,
+                          nightMode: true,
+                        );
+                        // get current locator
+                        VocsyEpub.locatorStream.listen((locator) {
+                          print('LOCATOR: $locator');
+                        });
+                        await VocsyEpub.openAsset(
+                          'assets/1.epub',
+                          lastLocation: EpubLocator.fromJson({
+                            "bookId": "2239",
+                            "href": "/OEBPS/ch06.xhtml",
+                            "created": 1539934158390,
+                            "locations": {
+                              "cfi": "epubcfi(/0!/4/4[simple_book]/2/2/6)"
+                            }
+                          }),
+                        );
+                      },
+                      child: Text('Open Assets E-pub'),
+                    ),
+                  ],
+                ),
+        ),
+      ),
+    );
+  }
+
+  Future downloadFile() async {
+    if (await Permission.storage.isGranted) {
+      await Permission.storage.request();
+      await startDownload();
+    } else {
+      await startDownload();
+    }
+  }
+
+  startDownload() async {
+    Directory? appDocDir = Platform.isAndroid
+        ? await getExternalStorageDirectory()
+        : await getApplicationDocumentsDirectory();
+
+    String path = appDocDir!.path + '/chair.epub';
+    File file = File(path);
+
+    if (!File(path).existsSync()) {
+      await file.create();
+      await dio.download(
+        "https://vocsyinfotech.in/envato/cc/flutter_ebook/uploads/22566_The-Racketeer---John-Grisham.epub",
+        path,
+        deleteOnError: true,
+        onReceiveProgress: (receivedBytes, totalBytes) {
+          print((receivedBytes / totalBytes * 100).toStringAsFixed(0));
+          setState(() {
+            loading = true;
+          });
+        },
+      ).whenComplete(() {
+        setState(() {
+          loading = false;
+          filePath = path;
+        });
+      });
+    } else {
+      setState(() {
+        loading = false;
+        filePath = path;
+      });
+    }
+  }
+}
